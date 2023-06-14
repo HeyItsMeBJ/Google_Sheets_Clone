19fdaad757a87fa64a8eb9d258a19af41b285213
Bhupesh Jain <104828846+HeyItsMeBJ@users.noreply.github.com>
2023-06-14 08:51:32 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+
+for (let alpha = 1; alpha <= 100; alpha++) {
+    var rowBar = document.querySelector('#rowBar');
+    var temp = document.createElement('div');
+    temp.classList.add('rowBarCells');
+    temp.setAttribute('id', `${alpha}`);
+    temp.innerHTML = alpha;
+    rowBar.appendChild(temp);
+
+}
+
+for (let alpha = 'A'; alpha <= 'Z'; alpha = String.fromCharCode(alpha.charCodeAt(0) + 1)) {
+    var columnBar = document.querySelector('#columnBar');
+    var temp = document.createElement('div');
+    temp.classList.add('columnBarCells');
+    temp.setAttribute('id', `${alpha}`);
+    temp.innerHTML = alpha;
+    columnBar.appendChild(temp);
+
+}
+for (let n = 1; n <= 100; n++) {
+    var columnBar = document.querySelector('#columnBar');
+    var rowBar = document.querySelector('#rowBar');
+    var sheetBox = document.querySelector('#sheetBox')
+    var temprow = document.createElement('div');
+    temprow.classList.add('rowNo');
+    temprow.setAttribute('id', `${n}`);
+    for (let alpha = 'A'; alpha <= 'Z'; alpha = String.fromCharCode(alpha.charCodeAt(0) + 1)) {
+        var temp = document.createElement('div');
+        temp.classList.add('sheetCells');
+        temp.setAttribute('id', `${alpha}${n}`);
+        temp.setAttribute('contenteditable', 'true');
+        // temp.innerHTML = alpha + n;
+        temprow.appendChild(temp);
+    }
+    sheetBox.appendChild(temprow);
+}
